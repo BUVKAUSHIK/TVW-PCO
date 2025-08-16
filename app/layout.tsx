bf1c8dba13d20a6759dccf635933d4595b178b3a
BUVKAUSHIK <BUVKAUSHIK@users.noreply.github.com>
2025-08-15 19:34:37 -0700

    fix: add missing stylesheet for react-day-picker

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import "react-day-picker/style.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
